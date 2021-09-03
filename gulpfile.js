c04cb333f2681a379426a23564e86fc1475b3128
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-09-03 17:13:33 +0100

    Add some basic styling
     * Use (for now) the styles and wrapper elements from the mibb-nibb website
     * Add sass and refactored gulpfile
     * Add some basic custom styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,49 @@ var source = require("vinyl-source-stream");
 var watchify = require("watchify");
 var tsify = require("tsify");
 var fancy_log = require("fancy-log");
+var sass = require('gulp-sass')(require('sass'));
 var paths = {
+  ts: ["src/main.ts"],
   pages: ["src/*.html", "src/includes/*.html"],
+  styles: ["src/scss/*.scss"]
 };
-var watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["src/main.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
 
-gulp.task("copy-html", function () {
+function style() {
+  return (
+    gulp.src(paths.styles)
+    .pipe(sass())
+    .on("error", fancy_log)
+    .pipe(gulp.dest('dist'))
+  );
+}
+
+function copyHtml() {
   return gulp.src(paths.pages).pipe(fileinclude({
     prefix: '@@',
     basepath: '@file'
   })).pipe(gulp.dest("dist"));
-});
+}
+
 function bundle() {
-  return watchedBrowserify
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: paths.ts,
+    cache: {},
+    packageCache: {},
+  }).plugin(tsify)
     .bundle()
     .on("error", fancy_log)
     .pipe(source("bundle.js"))
     .pipe(gulp.dest("dist"));
 }
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
+
+function watch() {
+  gulp.watch(paths.styles, style);
+  gulp.watch(paths.pages, copyHtml);
+  gulp.watch(paths.ts, bundle);
+}
+
+gulp.task("default", gulp.series(style, copyHtml, bundle));
+
+exports.watch = watch
