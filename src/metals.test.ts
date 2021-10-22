9d9ffeb5b0abaedc05d218c75b63e9662dc66b66
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-10-22 15:17:48 +0100

    Add tests for ranges of properties; update IsCloseTo checks to use SF

--- src/metals.test.ts
+++ src/metals.test.ts
@@ -1,4 +1,14 @@
-import { all_metals, calculateOccupancy } from './metals';
+import { all_metals, calculateOccupancy, Metal } from './metals';
+
+function checkIsCloseTo(value: number, expectedValue: number) {
+  // Use IsCloseTo but to 10 significant figures
+  let sigAfterDecimal = 10 - Math.floor((Math.log(expectedValue)/Math.log(10)));
+  // Don't go too deep (e.g. for 0-ish values)
+  if (sigAfterDecimal > 15) {
+    sigAfterDecimal = 15;
+  }
+  expect(value).toBeCloseTo(expectedValue, sigAfterDecimal);
+}
 
 test('calculateOccupancy', () => {
   var occupancies = calculateOccupancy();
@@ -17,7 +27,7 @@ test('calculateOccupancy', () => {
   expect(occupancies.keys).toEqual(expectedOccupancies.keys);
 
   for (var id in expectedOccupancies) {
-     expect(occupancies[id]).toBeCloseTo(expectedOccupancies[id]);
+    checkIsCloseTo(occupancies[id], expectedOccupancies[id]);
   }
 
   // Change some values and check again
@@ -29,19 +39,41 @@ test('calculateOccupancy', () => {
   occupancies = calculateOccupancy();
   // Expected values are copied from original spreadsheet
   expectedOccupancies = {
-    "mg": 0.997285259791523000,
-    "mn": 0.000000096034876869,
-    "fe": 0.000017729515729627,
-    "co": 0.000012312163701130,
+    "mg": 0.997285355565690000,
+    "mn": 0.000000000000009603,
+    "fe": 0.000017729517432279,
+    "co": 0.000012312164883527,
     "ni": 0.000000000000000000,
-    "cu": 0.000001846824555169,
-    "zn": 0.002313390758580710,
-    "total": 0.999630635088966000,
+    "cu": 0.000001846824732529,
+    "zn": 0.002313390980746910,
+    "total": 0.999630635053494000,
   };
 
   expect(occupancies.keys).toEqual(expectedOccupancies.keys);
 
   for (var id in expectedOccupancies) {
-     expect(occupancies[id]).toBeCloseTo(expectedOccupancies[id]);
+    checkIsCloseTo(occupancies[id], expectedOccupancies[id]);
   }
+});
+
+test('metalValueRanges', () => {
+  // Create a new metal
+  var m = new Metal("MyNewlyDiscoveredMetal", "My", 1, 1);
+
+  // Set affinity and bmc to extreme ends of valid values
+  m.affinity = 1000;
+  expect(m.affinity).toEqual(1000);
+  m.affinity = 1e-30;
+  expect(m.affinity).toBeCloseTo(1e-30, 32);
+
+  m.buffered_metal_concentration = 1000;
+  expect(m.buffered_metal_concentration).toEqual(1000);
+  m.buffered_metal_concentration = 1e-30;
+  expect(m.buffered_metal_concentration).toBeCloseTo(1e-30, 32);
+
+  // Set affinity outside of range
+  expect(() => m.affinity = 1001).toThrow(RangeError);
+  expect(m.affinity).toBeCloseTo(1e-30, 32);
+  expect(() => m.affinity = 0).toThrow(RangeError);
+  expect(m.affinity).toBeCloseTo(1e-30, 32);
 });
