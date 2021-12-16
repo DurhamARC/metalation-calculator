edbe2e9dbf323c69066fa6b34c3cbc684d0b04be
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-12-16 13:47:03 +0000

    npm build

--- metalation-calculator-wp/build/index.asset.php
+++ metalation-calculator-wp/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'db27baa4710300196ee2972864e07b87');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '8c9166fda0409a81a983c16469aff6c1');
