94bd277b3c97391d2819f6affc2d5f95ab0a5df2
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-01-14 14:34:48 +0000

    Add spacing in and around number inputs

--- metalation-calculator-wp/build/index.asset.php
+++ metalation-calculator-wp/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'c893dc45fb393678d9e0482721ea1f9d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '57e259768d961ba4563264c3ba7182c6');
