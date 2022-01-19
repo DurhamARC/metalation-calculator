b6bc1d4b5af976aea4d22cabbece2c6314b250bc
zeynepaki <73976500+zeynepaki@users.noreply.github.com>
2022-01-19 11:59:28 +0000

    `gulp wp` and `npm run build`

--- metalation-calculator-wp/build/index.asset.php
+++ metalation-calculator-wp/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f976e172ca4a8ea08a90a4d612d576e5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'd1309d65b2a8533c1d549b9a0bb3634a');
